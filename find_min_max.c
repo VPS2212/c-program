7c061e418d994be33234fdf1fd97add31d6bf3f3
VPS2212 <126179069+VPS2212@users.noreply.github.com>
2023-04-22 23:59:34 +0530

    Update find_min_max.c

--- find_min_max.c
+++ find_min_max.c
@@ -8,31 +8,31 @@ int main()
 		printf("Input\n");
 	for(i=0;i<size;i++)
 	{
-		scanf("%d",&ar[i]);
+		scanf("%d",&ar[i]); //get input form user
 	}
 	for(i=0;i<size;i++)
 	{
-		printf("%d\t",ar[i]);
+		printf("%d\t",ar[i]); //print input elements
 	}
-	max=ar[0];
-	for(i=0+1;i<size-1;i++)
+	max=ar[0]; // initialy set maxmimum value ar[0] , ex-ar[0]=10, max=10.
+	for(i=0+1;i<size-1;i++)  //loop start with ar[i+1], becuase we set ar[0] is max. 
 	{
-		if(max<ar[i+1])
+		if(max<ar[i+1]) //check maximum value to other element in array,check  other elements  whether big or not, if big update max =ar[i+1]
 		{
-		max=ar[i+1];
+		max=ar[i+1];//update max as ar[i+1], else the max value to be ar[0]. 
 		}	
-	}
-	min=ar[0];
+	} 
+	min=ar[0]; // initially set minimum value ar[0], 
 	for(j=0+1;j<size-1;j++)
 	{
-		if(min>ar[j+1])
+		if(min>ar[j+1]) //find minimum value 
 		{
-			min=ar[j+1];
+			min=ar[j+1]; //update minimum value if ar[0]>ar[i+1]
 		}
 	}
 	printf("\n************\n");
 	printf("Output\n");
-	printf("Max:%d\n",max);
+	printf("Max:%d\n",max); 
 	printf("Min:%d\n",min);
 	return 0;
 }
